1ea61ead77394b36a4e24e865737fa1e76c8b55c
Laiba <laibaara0756@gmail.com>
2026-09-08 15:08:15 +0500

    db issue

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB Connected!!");
-    } catch (e) {
-        console.log("Connection failed due to " + e);
-        process.exit(1);
-    }
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (e) {
+    console.error("MongoDB Connection failed:", e.message);
+  }
 };
 
 export default connectDB;
